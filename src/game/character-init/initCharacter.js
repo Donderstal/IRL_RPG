919cab8f55ff1923f31fd51cc17faa586b98a78a
Donderstal <daanonderstal@hotmail.com>
2019-10-17 11:59:54 +0200

    some more  review issues addressed

--- src/game/character-init/initCharacter.js
+++ src/game/character-init/initCharacter.js
@@ -5,10 +5,11 @@ const classList = Classes.initClasses
 module.exports = {
     // entry point to create a class with associated logic. Called from createCharInstance
     getCharWithClass : ( className, name, gender ) => {
-        for ( var key in classList ) { 
+        for ( let key in classList ) { 
             if ( key === className ) {
                 return new classList[className](name, gender)
             }
         }
+
     }
 }
--- src/game/createCharInstance.js
+++ src/game/createCharInstance.js
@@ -3,11 +3,11 @@ const initGamePiece = require('./overworld-init/initGamePiece')
 
 module.exports = {
     getCharacter : ( className, name, gender ) => {
-
         return {
             
             // entry point for creating a character
             // is called from GfxContainer.svelte
+            
 
             characterState : initChar.getCharWithClass( className, name, gender ),
             characterPiece : initGamePiece.initGamePiece(37, 37, 37)               
