fdfe88ce2ed8a36e6336f1a842c746056154b5e1
Donderstal <daanonderstal@hotmail.com>
2019-10-27 12:04:21 +0100

    experimenting with background grid generation

--- src/game/overworld-init/getGrid.js
+++ src/game/overworld-init/getGrid.js
@@ -1,3 +1,24 @@
+const overworlds = require('../../game-data/overworlds.json')
+const globals   = require('../../game-data/globals')
+
+const gridGetter = (gridName) => {
+    const newOverworld  = overworlds[gridName]
+    const dimensions    = getDimensionsInPixels(newOverworld)
+
+    console.log(dimensions)
+}
+
+const getDimensionsInPixels = (newOverworld) => {
+    const overworldDimensions   = newOverworld.dimensions
+
+    return {
+        horizontalPX  : overworldDimensions["horizontal"] * globals.GRID_BLOCK_PX, 
+        verticalPX    : overworldDimensions["vertical"] * globals.GRID_BLOCK_PX         
+    }
+}
+
+gridGetter('overworld-a')
+
 // Overworld grid pseudo-code
 
 // Json brainstorm
@@ -17,19 +38,30 @@
  *          vert-blocks: INT
  *      },
  *      border-blocks: {
- *         //store coordinates 
+ *          //store coordinates to define the borders of the overworld
+ *          //not yet sure if this is the right place to do so
+ *          //or how I would organise this
  *      },
  *      doors: {
+ *          // Doors should lead to other overworlds or to houses
+ *          // Their coordinates are stored here
  *          'D10' : house-1,
  *          'H12' : overworld-2
  *      },
  *      inaccessible: {
- *          A4,
- *          B4,
- *          C4
+ *          // This should store which tiles in the background are inaccessible
+ *          // Like walls an water and stuff like that
+ *          // Since the background will be a static image, I must define inaccessible tiles somewhay
+ *          possibly: [ A4, A12, D4, D12 ]
+ *          or: 'id' : {
+ *              type: square,
+ *              dims: [ A4, A12, D4, D12 ]
+ *          }
  *      },
  *      triggers; {
- * 
+ *      // overworlds will contain events
+ *      // they should be stored within the overworld object
+ *      // not sure how to do this yet
  *      },
  *      NPCs: {
  *      // I probably want to store information on NPCs here
@@ -50,3 +82,5 @@
  *      }
  * }
  */
+
+console.log('bruh')
