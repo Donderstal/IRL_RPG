ea0cb27b433c048f437fef233e64ac8c622fc380
Donderstal <daanonderstal@hotmail.com>
2020-06-25 13:25:52 +0200

    adjusting ui element's to Bart's designs

--- client/src/game-container/MainContainer.svelte
+++ client/src/game-container/MainContainer.svelte
@@ -1,7 +1,7 @@
 <script>
     import StartGameModal from './svelte-partials/StartGameModal.svelte'
     import GfxContainer from './game-gfx-container/GfxContainer.svelte'
-    import Header from './header/Header.svelte'
+    import Buttons from './buttons/Buttons.svelte'
 </script>
 
 <style>
@@ -17,10 +17,45 @@
         display: none;
     }
 
+    div {        
+        background-color: transparent;
+    }
+
+    h1 {
+        font-size: 120px;
+        font-family: 'Stormfaze', Fallback, sans-serif;
+        font: Regular 120px/144px Stormfaze;
+        letter-spacing: 0px;
+        opacity: 1;
+        width: 100vw;
+        margin-top: 0;
+    }
+
+    .main-header {
+        color: #D82BBA;
+        z-index:1;
+        display: absolute;
+        left: 12px;
+        top: 12px;
+    }
+
+    .shadow-header {
+        left: -6px;
+        top: -6px;
+        color: #00FF9E;
+        position: absolute;
+        z-index: 3;
+        display: absolute;
+    }
+
 </style>
 
 <div class="main-container" >
-    <Header/>
+    <div> 
+        <h1 class="main-header"> NECKBEARD 2020 </h1> 
+        <h1 class="shadow-header"> NECKBEARD 2020 </h1> 
+    </div>
+    <Buttons/>
     <GfxContainer/>
 
     <StartGameModal/>
--- client/src/game-container/svelte-partials/main-ui-button.svelte
+++ client/src/game-container/svelte-partials/main-ui-button.svelte
@@ -10,24 +10,43 @@
 
 <style>
     button {
-        background-color: #464F51;
+        background: #D82BBA 0% 0% no-repeat padding-box;
+        opacity: 1;
         color: #defff2;
-        border: 1px groove #D68Fd6;
-        border-radius: 5px;
-        width: 7vw;
-        height: 12.5vh;
+        width: 13.7vw;
+        height: 8.14vh;
         position: fixed;
-        left: 0;
+        left: 43.072916666vw;
+        font-family: 'Retro Gaming', Fallback, sans-serif;
+        font-size: 36px;
+        border: none;
+        z-index: 5;
+    }
+
+    p {
+        background: #64005380 0% 0% no-repeat padding-box;
+        opacity: 1;
+        width: 13.7vw;
+        height: 8.14vh;
+        position: fixed;
+        left: 43.38vw;
+        border: none;
+        display: block;
+        z-index: 4;
     }
 
     button:hover {
-        color: #D68Fd6;
-        border-color:  #defff2;
+        color: #00FF9E;
         cursor: pointer;
     }
 </style>
 
-<button style="top: {vhFromTop}" on:click={() => dispatch('buttonPress')} 
-        id={elementId} class="main-menu-button" >  
-    {buttonText} 
-</button>
+<div>
+    <button style="top: {vhFromTop}" on:click={() => dispatch('buttonPress')} 
+            id={elementId} class="main-menu-button" >  
+        {buttonText} 
+    </button>
+    <p style="top: {vhFromTop}">
+    
+    </p>
+</div>
