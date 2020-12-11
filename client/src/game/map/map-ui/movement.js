0eb7948dae2cd8d15b231a7fbe3d3e26ab1b59fc
Donderstal <daanonderstal@hotmail.com>
2020-12-11 15:55:29 +0100

    improving border collision detection

--- client/src/game/map/map-ui/movement.js
+++ client/src/game/map/map-ui/movement.js
@@ -32,7 +32,7 @@ const moveInDirection = ( sprite, direction ) => {
     const changedDirection = sprite.direction != globals[direction] ;
     sprite.direction = globals[direction]   
 
-    const movementIsAllowed = movementChecker.checkIfMovementAllowed( sprite, direction )
+    const movementIsAllowed = checkIfMovementAllowed( sprite, direction )
     const movingToNeighbour = checkForNeighbours(sprite)
 
     if ( movementIsAllowed && !movingToNeighbour && !changedDirection ) {
@@ -52,6 +52,46 @@ const moveInDirection = ( sprite, direction ) => {
         }     
     }
 }
+
+const checkIfMovementAllowed = ( sprite, direction ) => {
+    const activeMap = globals.GAME.activeMap;
+    const activeBackgroundTile = globals.GAME.back.class.grid.array[sprite.activeTileIndex];
+    const nextBackgroundTile = globals.GAME.back.class.grid.array[sprite.nextTileIndex];
+    const nextForegroundTile = globals.GAME.front.class.grid.array[sprite.nextTileIndex];
+
+    if ( activeBackgroundTile.row == 1 && direction == 'FACING_UP' 
+    && ( !activeMap.outdoors || !activeMap.neighbours.up ) ) {
+        return !sprite.isInCenterFacingUp;
+    }
+    else if ( activeBackgroundTile.row == globals.GAME.back.class.grid.rows && direction == 'FACING_DOWN' 
+    && ( !activeMap.outdoors || !activeMap.neighbours.down ) ) {
+        return !sprite.isInCenterFacingDown;
+    }
+    else if ( activeBackgroundTile.col == 1 && direction == 'FACING_LEFT' 
+    && ( !activeMap.outdoors || !activeMap.neighbours.left )  ) {
+        return !sprite.isInCenterFacingLeft;
+    }
+    else if ( activeBackgroundTile.col == globals.GAME.back.class.grid.cols && direction == 'FACING_RIGHT' 
+    && ( !activeMap.outdoors || !activeMap.neighbours.right )  ) {
+        return !sprite.isInCenterFacingRight;
+    }
+
+    if ( nextBackgroundTile != undefined && ( nextBackgroundTile.blocked || nextForegroundTile.hasSprite ) ) {
+        switch ( direction ) {
+            case 'FACING_RIGHT' :
+                return !sprite.isInCenterFacingRight;
+            case 'FACING_LEFT' :
+                return !sprite.isInCenterFacingLeft;
+            case 'FACING_UP' :
+                return !sprite.isInCenterFacingUp;
+            case 'FACING_DOWN' :
+                return !sprite.isInCenterFacingDown;
+        }
+    }
+    
+    return true
+}
+
 
 const checkForNeighbours = ( sprite ) => {
     const activeMap = globals.GAME.activeMap;
