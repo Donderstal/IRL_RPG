fd94712811d5d2e628b1717d2176b95bf8fc74f1
Donderstal <daanonderstal@hotmail.com>
2020-12-28 10:42:10 +0100

    splitting map classes to seperate files

---
+++ client/src/game/map/map-init/Door.js
@@ -0,0 +1,27 @@
+const globals       = require('../../../game-data/globals')
+const Sound         = require('../../interfaces/I_Sound').Sound
+
+class Door extends I_Hitbox {
+    constructor( x, y, door ) {
+        const radius = globals.GRID_BLOCK_PX;
+        super( x, y, radius)
+        this.to             = door.to;
+        this.directionIn    = door.directionIn
+        this.directionOut   = door.directionOut
+        this.locked         = door.locked
+        this.arcColor       = "#FFFFFF";
+
+    }
+
+    checkForBlockedRange( targetHitbox, targetDirection ) {
+        if ( super.checkForBlockedRange( targetHitbox, targetDirection ) ) {
+            globals.GAME.switchMap( this.to,"DOOR" );
+            const sfx = new Sound( "misc/random5.wav", true )
+            sfx.play()
+        }
+    }
+}
+
+module.exports = {
+    Door
+}
---
+++ client/src/game/map/map-init/MapObject.js
@@ -0,0 +1,52 @@
+const I_Hitbox      = require('../../interfaces/I_Hitbox').I_Hitbox
+const I_Sprite      = require('../../interfaces/I_Sprite').Sprite
+const globals       = require('../../../game-data/globals')
+const MapAction     = require('./MapAction').MapAction
+
+const mapObjectResources = require('../../../resources/mapObjectResources')
+
+class MapObject extends I_Sprite {
+    constructor ( tile ){
+        const objectResource = mapObjectResources[tile.spriteData.type]
+        const src = "/static/sprite-assets/" + objectResource.src
+        const dimensions = {
+            "width": objectResource.width_blocks * globals.GRID_BLOCK_PX,
+            "height": objectResource.height_blocks * globals.GRID_BLOCK_PX 
+        }
+
+        super( tile, dimensions, src )
+
+        this.widthInSheet   = objectResource.width_blocks * globals.GRID_BLOCK_IN_SHEET_PX;
+        this.heightInSheet  = objectResource.height_blocks * globals.GRID_BLOCK_IN_SHEET_PX;
+        this.hasAction  = tile.spriteData.hasAction;
+
+        if ( this.hasAction ) {
+            this.hitbox = new MapAction( this.x + (globals.GRID_BLOCK_PX * .25), this.y + (this.height - globals.GRID_BLOCK_PX), tile.spriteData.action )
+            this.action = tile.spriteData.action
+        }
+        else {
+            this.hitbox = new I_Hitbox( this.x + (globals.GRID_BLOCK_PX * .25), this.y + (this.height - globals.GRID_BLOCK_PX), this.width / 2 );
+        }
+    }
+
+    drawSprite( ) {
+        canvasHelpers.drawFromImageToCanvas(
+            "FRONT",
+            this.sheet,
+            this.sheetPosition * this.widthInSheet, 
+            this.direction * this.heightInSheet, 
+            this.widthInSheet, this.heightInSheet,
+            this.x, this.y, this.width, this.height
+        )
+
+        this.updateSpriteBorders( )
+
+        if ( this.hasAction ) {
+            this.hitbox.checkForActionRange( );        
+        }
+    }
+}
+
+module.exports = {
+    MapObject
+}
