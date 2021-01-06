c1c95c68ffd3f987a7cbd40f3900c0f8e43ad3e7
Donderstal <daanonderstal@hotmail.com>
2021-01-06 20:57:11 +0100

    adding some basic animation for driving cars

--- client/src/game/map/map-classes/MapObject.js
+++ client/src/game/map/map-classes/MapObject.js
@@ -28,14 +28,26 @@ class MapObject extends I_Sprite {
         else {
             this.hitbox = new I_Hitbox( this.x + (globals.GRID_BLOCK_PX * .25), this.y + (this.height - globals.GRID_BLOCK_PX), this.width / 2 );
         }
+
+        if ( tile.spriteData.moving ) {
+            this.movingToDestination = true;
+            this.destination = tile.spriteData.destination;
+            this.frames = objectResource["movement_frames"];
+            this.direction = globals[tile.spriteData.direction]
+            this.destinationTile = globals.GAME.front.class.grid.getTileAtCell( this.destination.row, this.destination.col )
+        }
     }
 
     drawSprite( ) {
+        if ( this.movingToDestination ) {
+            this.setActiveFrames( );
+        }
+
         canvasHelpers.drawFromImageToCanvas(
             "FRONT",
             this.sheet,
-            this.sheetPosition * this.widthInSheet, 
-            this.direction * this.heightInSheet, 
+            this.movingToDestination ? this.activeFrames[this.sheetPosition].x : this.sheetPosition * this.widthInSheet, 
+            this.movingToDestination ? this.activeFrames[this.sheetPosition].y : this.direction * this.heightInSheet, 
             this.widthInSheet, this.heightInSheet,
             this.x, this.y, this.width, this.height
         )
@@ -44,6 +56,77 @@ class MapObject extends I_Sprite {
 
         if ( this.hasAction ) {
             this.hitbox.checkForActionRange( );        
+        }
+
+        if ( this.movingToDestination ) {
+            this.goToDestination( );
+            this.countFrame( );
+        }
+    }
+
+    goToDestination( ) {
+        const destIsLeftOfSprite = (this.destinationTile.x - this.width) < this.left;
+        const destIsRightOfSprite = ( this.destinationTile.x + globals.GRID_BLOCK_PX + this.width ) > this.right;
+        const destIsAboveSprite = this.destinationTile.y - this.height < this.top;
+        const destIsBelowSprite = this.destinationTile.y + globals.GRID_BLOCK_PX + this.height > this.bottom;
+
+        this.moving = false
+        this.hasMoved = true;
+        if ( destIsLeftOfSprite && this.direction == globals["FACING_LEFT"] ) {
+            this.x -= globals.MOVEMENT_SPEED * 2;
+            this.moving = true;
+            this.direction = globals["FACING_LEFT"]
+        }
+        else if ( destIsAboveSprite && this.direction == globals["FACING_UP"] ) {
+            this.y -= globals.MOVEMENT_SPEED * 2;
+            this.moving = true;
+            this.direction = globals["FACING_UP"]
+        }
+        else if ( destIsRightOfSprite  && this.direction == globals["FACING_RIGHT"]  ) {
+            this.x += globals.MOVEMENT_SPEED * 2;
+            this.moving = true;
+            this.direction = globals["FACING_RIGHT"];
+        }
+        else if ( destIsBelowSprite && this.direction == globals["FACING_DOWN"]  ) {
+            this.y += globals.MOVEMENT_SPEED * 2;  
+            this.moving = true;
+            this.direction = globals["FACING_DOWN"]
+        }
+        
+        if ( !this.moving ) {
+            super.endGoToAnimation( );
+        }
+    }
+
+    setActiveFrames( ) {
+        switch ( this.direction ) {
+            case globals["FACING_LEFT"] :
+                this.activeFrames = this.frames["FACING_LEFT"];
+                break;
+            case globals["FACING_UP"] :
+                this.activeFrames = this.frames["FACING_UP"];
+                break;
+            case globals["FACING_RIGHT"] :
+                this.activeFrames = this.frames[ "FACING_RIGHT"];
+                break;
+            case globals["FACING_DOWN"] : 
+                this.activeFrames = this.frames[ "FACING_DOWN"];
+                break;
+            default :
+                break;
+        }  
+    }
+
+    countFrame( ) {
+        this.frameCount++;
+
+        if ( this.frameCount >= globals.FRAME_LIMIT) {
+            this.frameCount = 0;
+            this.sheetPosition++;
+
+            if ( this.sheetPosition >= this.activeFrames.length ) {
+                this.sheetPosition = 0;
+            }
         }
     }
 }
