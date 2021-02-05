3b03de8c23917db3959fbe1c30c68383b807229c
Donderstal <daanonderstal@hotmail.com>
2021-02-05 21:45:02 +0100

    cleaning and debugging pathfinding

--- client/src/helpers/pathfindingHelpers.js
+++ client/src/helpers/pathfindingHelpers.js
@@ -1,5 +1,20 @@
 const globals = require('../game-data/globals')
 
+const TILE_STATUS_INVALID = "INVALID";
+const TILE_STATUS_BLOCKED = "BLOCKED";
+const TILE_STATUS_VISITED = "VISITED";
+const TILE_STATUS_VALID = "VALID";
+const TILE_STATUS_EMPTY = "EMPTY";
+const TILE_STATUS_GOAL = "GOAL";
+
+const DIRECTION_NORTH = "NORTH";
+const DIRECTION_EAST = "EAST";
+const DIRECTION_SOUTH = "SOUTH";
+const DIRECTION_WEST = "WEST";
+
+let colsInGrid;
+let rowsInGrid;
+
 /**
  * @function getOppositeDirection
  * 
@@ -48,100 +63,125 @@ const determinePath = ( startingTile, destinationTile ) => {
  * @function determineShortestPath
  * All props to gregtrowbridge.com for explaining and sharing this algorithm
  */
-const determineShortestPath = ( startingTile, grid ) => {
-    let location = new GridLocation( startingTile.row, startingTile.col, "START" )
+const determineShortestPath = ( startingTile, targetTile, grid ) => {
+    colsInGrid = grid.cols;
+    rowsInGrid = grid.rows;
+    let tileList = grid.array;
+
+    grid.array[targetTile.index] = new GridLocation( targetTile.row, targetTile.col, targetTile.index, "GOAL" )
+    let location = new GridLocation( startingTile.row, startingTile.col, startingTile.index, "START" )
     const queue = [ location ];
 
     while ( queue.length > 0 ) {
         const currentLocation = queue.shift( );
 
-        var newLocation = exploreInDirection( currentLocation, 'NORTH', grid )
-        if ( newLocation.status == 'GOAL' ) {
-            return newLocation.path;
-        }
-        else if ( newLocation.status == 'VALID' ) {
-            queue.push(newLocation);
-        }
-
-        var newLocation = exploreInDirection( currentLocation, 'EAST', grid )
-        if ( newLocation.status == 'GOAL' ) {
-            return newLocation.path;
-        }
-        else if ( newLocation.status == 'VALID' ) {
-            queue.push(newLocation);
+        if ( currentLocation.row != 1 ) {
+            var newLocation = exploreInDirection( currentLocation, DIRECTION_NORTH, tileList )
+            if ( newLocation.status == TILE_STATUS_GOAL ) {
+                return newLocation.path;
+            }
+            else if ( newLocation.status == TILE_STATUS_VALID ) {
+                queue.push(newLocation);
+            }            
         }
 
-        var newLocation = exploreInDirection( currentLocation, 'SOUTH', grid )
-        if ( newLocation.status == 'GOAL' ) {
-            return newLocation.path;
-        }
-        else if ( newLocation.status == 'VALID' ) {
-            queue.push(newLocation);
+        if ( currentLocation.column != colsInGrid ) {
+            var newLocation = exploreInDirection( currentLocation, DIRECTION_EAST, tileList )
+            if ( newLocation.status == TILE_STATUS_GOAL ) {
+                return newLocation.path;
+            }
+            else if ( newLocation.status == TILE_STATUS_VALID ) {
+                queue.push(newLocation);
+            }
+
         }
 
-        var newLocation = exploreInDirection( currentLocation, 'WEST', grid )
-        if ( newLocation.status == 'GOAL' ) {
-            return newLocation.path;
-        }
-        else if ( newLocation.status == 'VALID' ) {
-            queue.push(newLocation);
+        if ( currentLocation.row != rowsInGrid ) {
+            var newLocation = exploreInDirection( currentLocation, DIRECTION_SOUTH, tileList )
+            if ( newLocation.status == TILE_STATUS_GOAL ) {
+                return newLocation.path;
+            }
+            else if ( newLocation.status == TILE_STATUS_VALID ) {
+                queue.push(newLocation);
+            }            
         }
 
-        return false;
+        if  ( currentLocation.column != 1 ) {
+            var newLocation = exploreInDirection( currentLocation, DIRECTION_WEST, tileList )
+            if ( newLocation.status == TILE_STATUS_GOAL ) {
+                return newLocation.path;
+            }
+            else if ( newLocation.status == TILE_STATUS_VALID ) {
+                queue.push(newLocation);
+            }            
+        }
     }
+
+    return false;    
 }
 
 class GridLocation {
-    constructor( row, column, status = null ) {
+    constructor( row, column, index, status = null ) {
         this.row = row;
         this.column = column;
         this.path = [];
         this.status = status;
+        this.index = index;
     }
 }
-const getLocationStatus = ( location, grid ) => {
-    const rowsInGrid;
-    const colsInGrid;
-
-    if ( location.row < 0 || location.column < 0 || location.row > rowsInGrid || location.col > colsInGrid ) {
-        return "INVALID";
-    } else if ( grid[location] == "GOAL" ) {
-        return 'GOAL';
-    } else if ( grid[location] !== "EMPTY" ) {
-        return 'BLOCKED';
+const getLocationStatus = ( location, tileList ) => {
+    if ( location.row < 1 || location.column < 1 || location.row > rowsInGrid || location.col > colsInGrid ) {
+        return TILE_STATUS_INVALID;
+    } else if ( tileList[location.index].status == TILE_STATUS_GOAL ) {
+        return TILE_STATUS_GOAL;
+    } else if ( 
+        globals.GAME.getTileOnCanvasAtIndex( "FRONT", location.index ).isBlocked 
+        || globals.GAME.getTileOnCanvasAtIndex( "BACK", location.index ).isBlocked 
+        || tileList[location.index].status ==  TILE_STATUS_VISITED ) {
+        return TILE_STATUS_BLOCKED;
     } else {
-        return "VALID";
+        return TILE_STATUS_VALID;
     }
 }
 
-const exploreInDirection = ( currentLocation, direction, grid ) => {
+const exploreInDirection = ( currentLocation, direction, tileList ) => {
     let newPath = currentLocation.path.slice( );
     newPath.push(direction);
 
     let row = currentLocation.row;
-    let col = currentLocation.col;
+    let col = currentLocation.column;
 
     switch( direction ) {
-        case "NORTH" :
+        case DIRECTION_NORTH :
+            row -= 1;
             break;
-        case "EAST" :
+        case DIRECTION_EAST :
+            col += 1;
             break;
-        case "SOUTH" :
+        case DIRECTION_SOUTH :
+            row += 1;
             break;
-        case "WEST" :
+        case DIRECTION_WEST :
+            col -= 1;
             break;
         default:
             console.log('Direction ' + direction + " not recognized" );
     }
 
-    const newLocation = new GridLocation( row, col );
-    newLocation.status = getLocationStatus( newLocation, grid );
+    let index = getCellIndex( row, col )
+    const newLocation = new GridLocation( row, col, index );
+    newLocation.path = newPath;
+    newLocation.status = getLocationStatus( newLocation, tileList );
 
-    if (  newLocation.status === "VALID" ) {
-        grid[newLocation] == 'VISITED'
+    if ( newLocation.status === TILE_STATUS_VALID ) {
+        tileList[newLocation.index].status = TILE_STATUS_VISITED;
     }
 
     return newLocation;
+}
+
+const getCellIndex = ( row, column ) => {
+    return ( ( row * colsInGrid ) - ( colsInGrid - column ) ) - 1
 }
 
 module.exports = {
