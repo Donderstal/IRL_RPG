01e2da984fe72a96983db32194d7eca3a2e874bf
Donderstal <daanonderstal@hotmail.com>
2021-03-13 17:11:27 +0100

    commenting canvas with grid

--- client/src/game/interfaces/I_CanvasWithGrid.js
+++ client/src/game/interfaces/I_CanvasWithGrid.js
@@ -1,25 +1,36 @@
 const { I_Grid } = require('./I_Grid');
-
+/**
+ * The game at its core consists out of two HTML5 Canvases: the Background and Foreground.
+ * Both are instantiated as an extension of the base I_CanvasWithGrid class
+ * I_CanvasWithGrid contains core functionalities for interacting with the canvas context based on a grid system.
+ * These include locating and returning a tile from the grid and instantiating a new grid.
+ */
 class I_CanvasWithGrid {
     constructor( x, y, ctx ) {
         this.x = x;
         this.y = y;
         this.ctx = ctx;
     };
-
-    setDimensions( ) {
-        this.width = width;
-        this.height = height;
-    };
-
+    /**
+     * Instantiate a I_Grid with the given number of rows and columns and set it to the this.grid prop
+     * @param {Number} rows 
+     * @param {Number} cols 
+     */
     initGrid( rows, cols ) {
         this.grid       = new I_Grid( this.x, this.y, rows, cols, this.ctx );
     };
-
-    setTileGrid( gridToSet ) {
-        this.grid.setTileGridToArray( gridToSet )
+    /**
+     * Call I_Grid.setTileGridArray() to set the given list of tile indexes to the grid
+     * @param {*} tileIndexArray - Array of tile indexes representing a map
+     */
+    setTileGrid( tileIndexArray ) {
+        this.grid.setTileGridToArray( tileIndexArray )
     }
-
+    /**
+     * Instantiatie an Image Instance and call callback on it when it loads
+     * @param {String} src - Location of image on server staring from root folder
+     * @param {Function} callback - Callback to call after loading the image
+     */
     loadImageWithCallback( src, callback ) {
         this.sheetImage = new Image();
         this.sheetImage.src = src;
@@ -29,19 +40,32 @@ class I_CanvasWithGrid {
             boundCallback( );
         };
     }
-
+    /**
+     * Call I_Grid.clearGrid( ) to unset the active grid
+     */
     clearGrid( ) {
         this.grid.clearGrid( );
     };
-
+    /**
+     * Return the I_Tile instance at index
+     * @param {Number} index array index of the tile in the grid array
+     */
     getTileAtIndex( index ) {
         return this.grid.array[index];
     }
-
+    /**
+     * Return the I_Tile instance at xy position
+     * @param {Number} x position of tile on X axis in canvas
+     * @param {Number} y position of tile on Y axis in canvas
+     */
     getTileAtXY( x, y ) {
         return this.grid.getTileAtXY( x, y );
     };
-
+    /**
+     * Return the I_Tile instance at column-row position
+     * @param {Number} column column of tile in canvas
+     * @param {Number} row position of tile on Y axis in canvas
+     */
     getTileAtCell( column, row ) {
         return this.grid.getTileAtCell( column, row );
     };
