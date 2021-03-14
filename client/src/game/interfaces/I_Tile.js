fd076bba8c49870a9d0ee12a279c51cfd6bf71e7
Donderstal <daanonderstal@hotmail.com>
2021-03-14 12:29:31 +0100

    adding comments to I_Tile

--- client/src/game/interfaces/I_Tile.js
+++ client/src/game/interfaces/I_Tile.js
@@ -2,7 +2,13 @@ const { GRID_BLOCK_PX, GRID_BLOCK_IN_SHEET_PX, SHEET_XY_VALUES } = require('../.
 const { MapAction } = require('../map/map-classes/MapAction')
 const { Door } = require('../map/map-classes/Door')
 const globals = require('../../game-data/globals')
-
+/**
+ * The I_Tile class is the most basic building block of the game.
+ * Each map is divided up in a grid of rows and columns with an I_Grid instance.
+ * Each cell in that map is represented by a I_Tile instance with an array-index and xy position.
+ * The I_Tile stores information about a tile's state and interactivity
+ * This could be the presence of a door or sprite or wether a tile can be crossed by sprites
+ */
 class I_Tile {
     constructor( index, x, y, ctx, row, col ) {
         this.x = x;
@@ -26,21 +32,31 @@ class I_Tile {
 
         this.clearTileID( );
     };
-
+    /**
+     * Return true if the Tile has an event of the type DOOR
+     */
     get hasDoor( ) { 
         return this.hasEvent && this.eventType == "DOOR"; 
     };
+    /**
+     * Return true if the tile is blocked or has a sprite on it
+     */
     get isBlocked( ) { 
         return this.blocked || this.hasSprite
     }
-
+    /**
+     * If this.ID is not null or empty, fetch the xy position of the this.ID property from SHEET_XY_VALUES.
+     * Draw the tile from the sheetImage to the UTIL canvas with drawTileToUtilityCanvas().
+     * Then draw the I_Tile at the position of this.x and this.y props
+     * @param {Image} sheetImage JS Image instance containing the current tilesheet png
+     */
     drawTileInMap( sheetImage ) {
         if ( this.ID === "E" || this.ID === null) {
             return;
         }
 
         const tilesheetXy = SHEET_XY_VALUES[this.ID]
-        this.flipTileBeforeDrawing( sheetImage, tilesheetXy );
+        this.drawTileToUtilityCanvas( sheetImage, tilesheetXy );
     
         this.ctx.drawImage(
             globals.GAME.util.canvas, 
@@ -50,8 +66,13 @@ class I_Tile {
             GRID_BLOCK_PX, GRID_BLOCK_PX
         )
     }
-
-    flipTileBeforeDrawing( sheetImage, tilesheetXy ) {
+    /**
+     * Draw the tile at given XY location in tilesheet to the Utility canvas.
+     * Depending on this.mirrored and this.angle property, mirror or flip the utility canvas.
+     * @param {Image} sheetImage JS Image instance containing the current tilesheet png
+     * @param {Object} tilesheetXy object with x and y Number properties
+     */
+    drawTileToUtilityCanvas( sheetImage, tilesheetXy ) {
         const ctx = globals.GAME.util.ctx;
         this.mirrored ? ctx.setTransform( -1, 0, 0, 1, GRID_BLOCK_IN_SHEET_PX, 0 ) : ctx.setTransform(1,0,0,1,0,0);
         switch( this.angle ) {
@@ -99,7 +120,12 @@ class I_Tile {
                 alert('Error in flipping tile. Call the police!')
         }
     }
-
+    /**
+     * Set this.hasEvent property to true and assign type to this.eventType.
+     * Then, set the Event to the I_Tile with a method depending on the type argument
+     * @param {String} type 'enumerable' that decided how to set the given eventData. DOOR || ACTION
+     * @param {Object} eventData A door or action object from the doors or actions in the active map
+     */
     setEventData( type, eventData ) {
         this.hasEvent = true;
         this.eventType = type;
@@ -112,7 +138,11 @@ class I_Tile {
                 break;
         }
     }
-
+    /**
+     * Calculate the new doors' XY depending on the directionIn prop of doorData
+     * Then, instantiate a Door instance and assign it to the event property
+     * @param {Object} doorData object from the doors array in the current map
+     */
     setDoor( doorData ) {
         const directionIn = doorData.directionIn
         let xy = { };
@@ -135,41 +165,58 @@ class I_Tile {
                 break;
         }
         this.event = new Door( xy.x, xy.y, doorData );
-        //setTimeout( ( ) => { this.event.draw( xy.x, xy.y ); }, 1000)
     }
-
+    /**
+     * Instantiate a Action instance and assign it to the event property
+     * @param {Object} doorData object from the doors array in the current map
+     */
     setAction( actionData ) {
         this.event = new MapAction( this.x + ( GRID_BLOCK_PX / 2 ), this.y + ( GRID_BLOCK_PX / 2 ), actionData );
-        //setTimeout( ( ) => { this.event.draw( this.x + ( GRID_BLOCK_PX / 2 ), this.y + ( GRID_BLOCK_PX / 2 ) ); }, 1000)
     }
-
+    /**
+     * Set this.hasEvent to false and this.event to null 
+     */
     clearEventData( ) {
         this.hasEvent = false;
         this.event = null;
     }
-
+    /**
+     * Set this.hasSprite property to true and assign type to this.spriteType. Store data parameter in this.spriteData
+     * @param {String} type 'enumerable' that decided how to set the given sprite data. 'character' || 'object'
+     * @param {Object} data A character or mapObject object from the characters or mapObjects in the active map
+     */
     setSpriteData( type, data ) {
         this.hasSprite = true;
         this.spriteType = type;
         this.spriteData = data;
     }
-
+    /**
+     * Clear all sprite related properties: hasSprite, spriteType, spriteData and spriteId
+     */
     clearSpriteData( ) {
         this.hasSprite = false;
         this.spriteType = null;
         this.spriteData = null;
         this.spriteId = null;
     }
-
+    /**
+     * Set wether the tilesheet tile in the I_Tile instance should be drawn flipped or mirrored
+     * @param {Object} settings with the properties mirrored and angle 
+     */
     setSettings( settings ) {
         this.mirrored = settings['mirrored'];
         this.angle = settings['angle'];
     }
-
+    /**
+     * Set given as this.ID. The ID property decides which tile from the tilesheet will be drawn
+     * @param {Number} ID 
+     */
     setTileID( ID ) {
         this.ID = ID;
     };
-
+    /**
+     * Set this.ID to empty, meaning that no tilesheet tile will de drawn in this I_Tile instance
+     */
     clearTileID( ) {
         this.ID = "E"
     };
