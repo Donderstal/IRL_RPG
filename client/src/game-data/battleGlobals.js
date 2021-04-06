859754a526bb80b29322688b8e3d09185f162bed
Donderstal <daanonderstal@hotmail.com>
2021-04-06 15:02:14 +0200

    battle position globals

--- client/src/game-data/battleGlobals.js
+++ client/src/game-data/battleGlobals.js
@@ -3,7 +3,29 @@ const { STRD_SPRITE_HEIGHT, STRD_SPRITE_WIDTH } = require('./globals')
 const BATTLE_SPRITE_WIDTH   = STRD_SPRITE_WIDTH * 3
 const BATTLE_SPRITE_HEIGHT  = STRD_SPRITE_HEIGHT * 3
 
+const LEFT_BATTLE_POSITION_1    = { 'column': 4, 'row': 7 };
+const LEFT_BATTLE_POSITION_2    = { 'column': 6, 'row': 9 };
+const LEFT_BATTLE_POSITION_3    = { 'column': 4, 'row': 11 };
+
+const RIGHT_BATTLE_POSITION_1   = { 'column': 20, 'row': 7 };
+const RIGHT_BATTLE_POSITION_2    = { 'column': 18, 'row': 9 };
+const RIGHT_BATTLE_POSITION_3    = { 'column': 20, 'row': 11 };
+
+const SHEET_ROW_BATTLE_LEFT         = 4
+const SHEET_ROW_BATTLE_RIGHT        = 5
+
 module.exports = {
     BATTLE_SPRITE_WIDTH,
-    BATTLE_SPRITE_HEIGHT
+    BATTLE_SPRITE_HEIGHT,
+
+    LEFT_BATTLE_POSITION_1,
+    LEFT_BATTLE_POSITION_2,
+    LEFT_BATTLE_POSITION_3,
+
+    RIGHT_BATTLE_POSITION_1,
+    RIGHT_BATTLE_POSITION_2,
+    RIGHT_BATTLE_POSITION_3,
+    
+    SHEET_ROW_BATTLE_LEFT,
+    SHEET_ROW_BATTLE_RIGHT
 }
