85839ecc0e29246d5edcd3a237217811195b2160
Donderstal <daanonderstal@hotmail.com>
2021-04-07 20:38:09 +0200

    setting up battle globals

---
+++ client/src/game/battle/Battle.js
@@ -0,0 +1,12 @@
+const { 
+    BATTLE_PHASE_BEGIN_TURN, BATTLE_PHASE_SELECT_MOVE, BATTLE_PHASE_DO_MOVES,
+    BATTLE_PHASE_END_TURN, BATTLE_PHASE_END_BATTLE
+} = require('../../game-data/battleGlobals');
+
+class Battle {
+
+}
+
+module.exports = {
+    Battle
+}
