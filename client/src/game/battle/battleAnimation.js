9722db313358ac5928ccfb4b97b7138dd0938bbe
Donderstal <daanonderstal@hotmail.com>
2021-04-27 09:36:57 +0200

    sorting sprites for depth

--- client/src/game/battle/battleAnimation.js
+++ client/src/game/battle/battleAnimation.js
@@ -1,5 +1,22 @@
+/**
+ * Sort active slots based on the position of their associated sprite in the grid.
+ * Then, draw them in sorted order to simulate depth.
+ * @param {Game} GAME active Game instance
+ */
 const handleBattleAnimations = ( GAME ) => {
-    GAME.FRONT.battleSlots.forEach( ( slot ) => { slot.drawSpriteInSlot( ); } );
+    const slots = [...GAME.FRONT.battleSlots];
+    slots.sort( ( a, b ) => {
+        if ( a.sprite.row > b.sprite.row || a.sprite.row === b.sprite.row && a.sprite.y > b.sprite.y ) {
+            return 1 
+        }
+        else if (b.sprite.row > a.sprite.row || b.sprite.row === a.sprite.row && b.sprite.y > a.sprite.y ) {
+            return -1
+        }
+        else {
+            return 0
+        }          
+    })
+    slots.forEach( ( slot ) => { slot.drawSpriteInSlot( ); } );
 }
 
 module.exports = { 
