ebcb5e42bde987d3aeb8cd5c1fd627bac7f93cc1
Donderstal <daanonderstal@hotmail.com>
2021-07-19 10:45:17 +0200

    splitting up downtown resource file

---
+++ client/src/resources/mapResources/downtown/A3/downtown-A3.js
@@ -0,0 +1,97 @@
+const { NPC_ANIM_TYPE_MOVING_IN_LOOP, NPC_MOVE_TYPE_FLYING }
+
+module.exports = {
+    "mapName": "downtown/A3",
+    "tileSet": "downtown",
+    "outdoors": true,
+    "music": "game-jam.mp3",
+    "roads" : [
+        { "alignment": "HORI", "topRow": 10, "bottomRow": 11, "direction": "FACING_LEFT" }
+      ],
+    "neighbours": {
+        "left": "downtown/A2",
+        "right": "downtown/A4"
+    },
+    "rows":12,
+    "columns":24,
+    "grid":[
+        [113,114,113,114,113,114,139,185,186,187,212,213,215,215,215,213,215,215,214,215,243,152,153,154],
+        [117,118,117,118,117,118,143,189,190,191,208,209,209,209,209,209,209,209,209,209,239,148,149,150],
+        [121,122,121,122,121,122,143,145,88,211,212,213,214,215,214,215,215,215,214,215,243,156,157,157],
+        [0,140,0,0,0,73,4,88,0,252,216,217,218,217,217,244,245,217,217,218,247,0,0,0],
+        [0,0,24,0,0,0,0,0,0,256,220,221,221,221,221,248,249,221,221,221,251,0,0,24],
+        [0,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,23,0,0,0,12],
+        [0,0,0,0,125,126,0,0,0,0,0,24,0,12,0,0,0,0,0,40,0,0,0,0],
+        [0,12,0,102,28,28,31,28,28,28,28,28,28,28,28,103,0,0,25,0,0,25,23,0],
+        [28,28,105,106,66,67,67,67,67,67,67,67,67,67,100,107,28,28,28,28,31,28,28,28],
+        [347,38,347,347,347,36,347,347,347,347,347,347,54,347,37,347,347,347,347,347,347,347,347,347],
+        [347,347,347,347,347,38,347,347,347,347,347,347,347,347,347,347,347,36,347,347,347,347,347,38],
+        [39,347,39,347,39,347,39,347,39,347,39,347,39,36,39,347,39,347,39,347,39,347,39,36]
+    ],
+    "mapObjects" : [
+        {
+            "type"  : "Bus_Stop",
+            "row"   : 8,
+            "col"   : 20,
+            "hasAction" : true,
+            "action" : {
+                "type": "BUS",
+                "to": "my-neighbourhood/A2",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Player",
+                      "sfx": "typing.mp3",
+                      "text": "Shall I take the bus to my neighbourhood?"
+                    }
+                  ]
+            }
+        },
+        { 
+            "type"  : "Sign_01",
+            "row"   : 2,
+            "col"   : 10
+        },
+        { 
+            "type"  : "Sign_02",
+            "row"   : 2,
+            "col"   : 8
+        },
+        { 
+            "type"  : "Sign_03",
+            "row"   : 2,
+            "col"   : 15
+        },
+        { 
+            "type"  : "Sign_04",
+            "row"   : 2,
+            "col"   : 17
+        }
+    ],
+    "characters" : [
+        {
+            "anim_type": NPC_ANIM_TYPE_MOVING_IN_LOOP,
+            "move_type": NPC_MOVE_TYPE_FLYING,
+            "sprite": "pigeon.png",
+            "direction": "FACING_DOWN", 
+            "row": 6,
+            "col": 8,
+            "destination" : {
+                "row": 2,
+                "col": 13,
+            },
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_MOVING_IN_LOOP,
+            "move_type": NPC_MOVE_TYPE_FLYING,
+            "sprite": "pigeon.png",
+            "direction": "FACING_DOWN",
+            "row": 1,
+            "col": 14,
+            "destination" : {
+                "row": 5,
+                "col": 18,
+            },
+        }
+    ]
+}
---
+++ client/src/resources/mapResources/downtown/A6/downtown-A6.js
@@ -0,0 +1,157 @@
+module.exports = {
+    "mapName": "downtown/A6",
+    "tileSet": "downtown",
+    "outdoors": true,
+    "music": "game-jam.mp3",
+    "roads" : [
+        { "alignment": "HORI", "topRow": 10, "bottomRow": 11, "direction": "FACING_LEFT" },
+        { "alignment": "VERT", "leftCol": 23, "rightCol": 24, "direction": "FACING_DOWN" }
+      ],
+    "neighbours": {
+        "left": "downtown/A5"
+    },
+    "rows":12,
+    "columns":24,
+    "grid":[[522,523,523,523,523,523,523,210,0,0,0,0,0,0,0,0,0,0,0,0,339,347,347,347],[526,527,540,541,542,543,556,328,73,0,518,0,0,559,572,518,0,0,0,0,147,347,347,347],[530,531,544,545,546,547,560,332,0,0,562,563,580,581,582,583,600,0,0,0,147,347,347,36],[534,535,548,549,550,551,564,0,561,0,566,567,584,585,586,587,604,0,569,0,147,347,347,347],[538,539,552,553,554,555,568,0,565,0,570,571,588,589,590,591,601,0,573,0,147,347,347,347],[536,0,516,0,0,57,0,0,0,0,574,575,592,593,594,595,602,0,0,73,147,347,347,347],[0,0,0,0,0,0,0,0,518,0,578,579,596,597,598,599,603,0,0,0,147,347,347,347],[0,438,439,102,28,103,0,96,0,0,0,0,0,576,577,518,0,0,40,4,147,347,347,347],[28,28,105,106,38,107,28,28,28,28,28,28,28,28,28,28,28,28,28,28,329,347,36,347],[347,347,347,347,347,347,347,347,347,347,66,67,67,67,67,67,67,67,67,347,347,347,347,347],[347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347,347],[347,39,347,39,347,39,347,39,347,39,347,39,347,39,347,39,347,39,347,39,347,39,347,39]],
+    "characters" : [
+        {
+            "anim_type": NPC_ANIM_TYPE_IDLE,
+            "sprite": "characterx5_recolour.png",
+            "direction": "FACING_DOWN",
+            "row": 3,
+            "col": 13,
+            "name": "Pidgebert thug 1",                     
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Pidgebert thug 1",  
+                      "sfx": "voice-1.mp3",
+                      "text": "If you mess with Pidgebert you mess with me!"
+                    }
+                ]
+            }
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_IDLE,
+            "sprite": "pigeon.png",
+            "direction": "FACING_DOWN",
+            "row": 3,
+            "col": 14,
+            "name": "Pidgebert",                    
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Pidgebert",
+                      "sfx": "voice-1.mp3",
+                      "text": "Who you lookin at, shithead?"
+                    },
+                    {
+                        type: "SPEAK",
+                        spriteName: "Pidgebert",
+                        "sfx": "voice-1.mp3",
+                        "text": "I'm Pidgebert, the meanest pigeon in town!"
+                    }
+                ]
+            }
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_IDLE,
+            "sprite": "fats_recolour.png",
+            "direction": "FACING_DOWN",
+            "row": 3,
+            "col": 15,
+            "name": "Pidgebert thug 2",                    
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Pidgebert thug 2",  
+                      "sfx": "voice-1.mp3",
+                      "text": "A powerful pigeon like Pidgebert needs protection."
+                    }
+                ]
+            }
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_MOVING,
+            "sprite": "pigeon.png",
+            "direction": "FACING_LEFT",
+            "name": "Marching pigeon 1",    
+            "row": 5,
+            "col": 14,
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Marching pigeon 1",     
+                      "sfx": "voice-1.mp3",
+                      "text": "Pigeons of the world, unite!"
+                    }
+                ]
+            }
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_MOVING,
+            "sprite": "pigeon.png",
+            "direction": "FACING_LEFT",
+            "name": "Marching pigeon 2",    
+            "row": 6,
+            "col": 14,
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Marching pigeon 2",   
+                      "sfx": "voice-1.mp3",
+                      "text": "I'm mad as hell and I'm not going to take it anymore"
+                    }
+                ]
+            }
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_MOVING,
+            "sprite": "pigeon.png",
+            "direction": "FACING_LEFT",
+            "name": "Marching pigeon 3",    
+            "row": 7,
+            "col": 14,
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Marching pigeon 3",    
+                      "sfx": "voice-1.mp3",
+                      "text": "Proud pigeons far and wide, march with us!"
+                    }
+                ]
+            }
+        },
+        {
+            "anim_type": NPC_ANIM_TYPE_SEMI_IDLE,
+            "sprite": "characterx3.png",
+            "direction": "FACING_DOWN",
+            "name": "Grandma Shanksville",    
+            "row": 8,
+            "col": 3,
+            "action": {
+                "type": "TEXT",
+                "scenes": [
+                    {
+                      type: "SPEAK",
+                      spriteName: "Grandma Shanksville",  
+                      "sfx": "voice-2.mp3",
+                      "text": "Pidgebert is not like other pigeons, if you understand what I mean."
+                    }
+                ]
+            }
+        }
+    ]
+}
