8c685159ab987f671446d0b372d632e62096679a
Donderstal <daanonderstal@hotmail.com>
2021-07-23 18:12:52 +0200

    cleaning and updating event res and scripts

--- client/src/resources/eventResources.js
+++ client/src/resources/eventResources.js
@@ -7,13 +7,13 @@ const chapterEvents = [
                 mapName: "my-neighbourhood/A1",
                 trigger: "ON_ENTER",
                 passScene: false,
-                scriptId: "C0_S0_E0"
+                scriptId: "0_0_0"
             },
             {
                 mapName: "my-neighbourhood/A2",
                 trigger: "ON_ENTER",
                 passScene: true,
-                scriptId: "C0_S0_E1"
+                scriptId: "0_0_1"
             }  
         ],
         // C 0, Scene 1
@@ -22,7 +22,7 @@ const chapterEvents = [
                 mapName: "my-neighbourhood/A3",
                 trigger: "ON_LEAVE",
                 passScene: true,
-                scriptId: "C0_S1_E0"
+                scriptId: "0_1_0"
             }
         ],
         // C 0, Scene 2
@@ -35,7 +35,7 @@ const chapterEvents = [
                     "direction": "FACING_RIGHT"
                 },
                 passScene: true,
-                scriptId: "C0_S2_E0"
+                scriptId: "0_2_0"
             }
         ],
         // C 0, Scene 3
@@ -44,7 +44,7 @@ const chapterEvents = [
                 mapName: "my-neighbourhood/A4/yum-mart",
                 trigger: "ON_ENTER",
                 passScene: false,
-                scriptId: "C0_S3_E0"
+                scriptId: "0_3_0"
             }
         ]         
     ]
--- client/src/resources/eventScripts.js
+++ client/src/resources/eventScripts.js
@@ -1,199 +1,138 @@
 const eventScripts = {
     // Chapter 0, Scene 0
-    "C0_S0_E0" : {
+    "0_0_0" : {
+        name: "0_0_0",
         scenes: [
             { 
-                type: "SPEAK",
-                spriteName: "Mr. Business",
-                text: "Hey!",
-                sfx: "voice-2.mp3"
+                type: "SPEAK", spriteName: "Mr. Business",
+                text: "Hey!", sfx: "voice-2.mp3"
             },
             { 
-                type: "MOVE",
-                spriteName: "Mr. Business",
-                destination: "Player",
-                endDirection: "FACING_UP"
+                type: "MOVE", spriteName: "Mr. Business",
+                destination: "Player", endDirection: "FACING_UP"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Mr. Business",
-                text: "Don't you work at our branch a few blocks down the street?",
-                sfx: "voice-2.mp3"
+                type: "SPEAK", spriteName: "Mr. Business",
+                text: "Don't you work at our branch a few blocks down the street?", sfx: "voice-2.mp3"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Mr. Business",
-                text: "You better hurry up! Your shift's already begun...",
-                sfx: "voice-2.mp3"
+                type: "SPEAK", spriteName: "Mr. Business",
+                text: "You better hurry up! Your shift's already begun...", sfx: "voice-2.mp3"
             },
             { 
-                type: "ANIM",
-                spriteName: "Mr. Business",
-                animName: "TURN_SINGLE_CIRCLE",
-                loop: false
+                type: "ANIM", spriteName: "Mr. Business",
+                animName: "TURN_SINGLE_CIRCLE", loop: false
             },
             { 
-                type: "SPEAK",
-                spriteName: "Mr. Business",
-                text: "See ya later, wage cuck!",
-                sfx: "lala.mp3"
+                type: "SPEAK", spriteName: "Mr. Business",
+                text: "See ya later, wage cuck!", sfx: "lala.mp3"
             },
             { 
-                type: "MOVE",
-                spriteName: "Mr. Business",
-                destination: {
-                    "row": 6,
-                    "col": 22
-                }
+                type: "MOVE", spriteName: "Mr. Business",
+                destination: { "row": 6, "col": 22 }
             }
         ]
     },
-    "C0_S0_E1" : {
+    "0_0_1" : {
+        name: "0_0_1",
         scenes: [
             { 
-                type: "SPEAK",
-                spriteName: "Chaddy Chandler",
-                text: "Hey Nerd!",
-                sfx: "battle-baba.mp3"
+                type: "SPEAK", spriteName: "Chaddy Chandler",
+                text: "Hey Nerd!", sfx: "battle-baba.mp3"
             },
             {
-                type: "MOVE",
-                spriteName: "Chaddy Chandler",
-                destination: {
-                    "row": 5,
-                    "col": 4
-                }
+                type: "MOVE", spriteName: "Chaddy Chandler",
+                destination: { "row": 5, "col": 4 }
             },
             { 
-                type: "MOVE",
-                spriteName: "Chaddy Chandler",
+                type: "MOVE", spriteName: "Chaddy Chandler",
                 destination: "Player",
             },
             { 
-                type: "SPEAK",
-                spriteName: "Chaddy Chandler",
-                text: "Come over here, will ya?",
-                sfx: "voice-1.mp3"
+                type: "SPEAK", spriteName: "Chaddy Chandler",
+                text: "Come over here, will ya?", sfx: "voice-1.mp3"
             },
             { 
-                type: "MOVE",
-                spriteName: "Chaddy Chandler",
-                destination: {
-                    "row": 3,
-                    "col": 5
-                },
-                endDirection: "FACING_DOWN"
+                type: "MOVE", spriteName: "Chaddy Chandler",
+                destination: { "row": 3, "col": 5 }, endDirection: "FACING_DOWN"
             },
             { 
-                type: "ANIM",
-                spriteName: "Chaddy Chandler",
-                animName: "TURN_SINGLE_CIRCLE",
-                endDirection: "FACING_DOWN",
-                loop: false
+                type: "ANIM", spriteName: "Chaddy Chandler",
+                animName: "TURN_SINGLE_CIRCLE", endDirection: "FACING_DOWN", loop: false
             }
         ]
     },
     // Chapter 1, Scene 0
-    "C0_S1_E0" : {
+    "0_1_0" : {
+        name: "0_1_0",
         scenes: [
             { 
-                type: "SPEAK",
-                spriteName: "Dirk Chan",
-                text: "Why would you want to work at Yum-Mart?",
-                sfx: "mauww.mp3"
+                type: "SPEAK", spriteName: "Dirk Chan",
+                text: "Why would you want to work at Yum-Mart?", sfx: "mauww.mp3"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Dirk Chan",
-                text: "My Universal Basic Income covers most of my expenses...",
-                sfx: "mauww.mp3"
+                type: "SPEAK", spriteName: "Dirk Chan",
+                text: "My Universal Basic Income covers most of my expenses...", sfx: "mauww.mp3"
             }
         ]
     },
     // Chapter 2, Scene 0
-    "C0_S2_E0" : {
+    "0_2_0" : {
+        name: "0_2_0",
         scenes: [
             { 
-                type: "SPEAK",
-                spriteName: "Player",
+                type: "SPEAK", spriteName: "Player",
                 text: "I'm almost there, I hope Bob doesn't get mad at me!"
             }
         ]
     },
     // Chapter 3, Scene 0
-    "C0_S3_E0" : {
+    "0_3_0" : {
+        name: "0_3_0",
         scenes: [
             { 
-                type: "SPEAK",
-                spriteName: "Manager Bob",
-                text: "Better late than never!",
-                sfx: "battle-baba.mp3"
+                type: "SPEAK", spriteName: "Manager Bob",
+                text: "Better late than never!", sfx: "battle-baba.mp3"
             },
             { 
-                type: "MOVE",
-                spriteName: "Manager Bob",
+                type: "MOVE", spriteName: "Manager Bob",
                 destination: "Player"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Manager Bob",
-                text: "Can we talk in the corner for a sec?",
-                sfx: "voice-1.mp3"
+                type: "SPEAK", spriteName: "Manager Bob",
+                text: "Can we talk in the corner for a sec?", sfx: "voice-1.mp3"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Player",
+                type: "SPEAK", spriteName: "Player",
                 text: "Sure...!"
             },
             { 
-                type: "MOVE",
-                spriteName: "Player",
-                destination: {
-                    row: 13,
-                    col: 1
-                },
-                endDirection: "FACING_RIGHT"
+                type: "MOVE", spriteName: "Player",
+                destination: { row: 13, col: 1  }, endDirection: "FACING_RIGHT"
             },
             { 
-                type: "MOVE",
-                spriteName: "Manager Bob",
-                destination: {
-                    row: 13,
-                    col: 2
-                },
-                endDirection: "FACING_LEFT"
+                type: "MOVE", spriteName: "Manager Bob",
+                destination: { row: 13, col: 2 }, endDirection: "FACING_LEFT"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Manager Bob",
-                text: "I know you've been going through some things lately...",
-                sfx: "voice-1.mp3"
+                type: "SPEAK", spriteName: "Manager Bob",
+                text: "I know you've been going through some things lately...", sfx: "voice-1.mp3"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Manager Bob",
-                text: "But you can't keep coming late. I'm sorry but it just wouldn't be fair to the other employees.",
-                sfx: "battle-baba.mp3"
+                type: "SPEAK", spriteName: "Manager Bob",
+                text: "But you can't keep coming late. I'm sorry but it just wouldn't be fair to the other employees.", sfx: "battle-baba.mp3"
             },
             { 
-                type: "SPEAK",
-                spriteName: "Player",
+                type: "SPEAK", spriteName: "Player",
                 text: "I'm sorry Bob', I'll make sure it won't happen again."
             },
             { 
-                type: "SPEAK",
-                spriteName: "Manager Bob",
-                text: "Please do, because I'm happy to have you. Now let's stock those shelves!",
-                sfx: "voice-1.mp3"
+                type: "SPEAK", spriteName: "Manager Bob",
+                text: "Please do, because I'm happy to have you. Now let's stock those shelves!", sfx: "voice-1.mp3"
             },
             { 
-                type: "MOVE",
-                spriteName: "Manager Bob",
-                destination: {
-                    row: 10,
-                    col: 6
-                },
-                endDirection: "FACING_DOWN"
+                type: "MOVE", spriteName: "Manager Bob",
+                destination: { row: 10, col: 6 }, endDirection: "FACING_DOWN"
             }
         ]
     },
