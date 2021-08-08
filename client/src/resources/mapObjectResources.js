081de118c999ce604c0afcbf902271093c9e9e54
Donderstal <daanonderstal@hotmail.com>
2021-08-08 21:12:30 +0200

    cleaning up object resources

--- client/src/resources/mapObjectResources.js
+++ client/src/resources/mapObjectResources.js
@@ -1,15 +1,69 @@
+const ONE_BLOCK_SPRITE = {
+    "dimensional_alignment": "STANDARD",
+    "height_blocks": 1, "width_blocks": 1
+}
+
+const TWO_WIDE_SPRITE = { 
+    "dimensional_alignment": "STANDARD",
+    "height_blocks": 1, "width_blocks": 2
+}
+
+const THREE_HIGH_SPRITE = {
+    "dimensional_alignment": "STANDARD",
+    "height_blocks": 3, "width_blocks": 1
+}
+
+const POSTER_SPRITE = {
+    "dimensional_alignment": "STANDARD", "not_grounded": true,
+    "height_blocks": 1.75, "width_blocks": 1.75,
+}
+
+const STANDARD_CAR = {
+    "dimensional_alignment": "HORI_VERT", "isCar" : true,
+    "hori_height_blocks": 3, "hori_width_blocks": 4,
+    "vert_height_blocks": 3, "vert_width_blocks": 2,
+    "movement_frames" : { 
+        "FACING_LEFT" : [
+            { "x": 0, "y": 384 },
+            { "x": 0, "y": 576 }
+        ],
+        "FACING_UP" : [
+            { "x": 0, "y": 960 },
+            { "x": 128, "y": 960 }
+        ],
+        "FACING_RIGHT" : [
+            { "x": 0, "y": 0 },
+            { "x": 0, "y": 192 }
+        ],
+        "FACING_DOWN" : [
+            { "x": 0, "y": 768 },
+            { "x": 128, "y": 768 }
+        ]
+    }
+}
+
+const getSignData = ( heightInBlocks ) => {
+    return {
+        "dimensional_alignment": "STANDARD",
+        "height_blocks": heightInBlocks,
+        "width_blocks": 1,
+        "not_grounded": true,
+        "idle_animation": true
+    }
+}
+
 module.exports = {
+    "bench_a" :{
+        "src": "bench_a.png",
+        ...TWO_WIDE_SPRITE
+    },
     "Bench_Green" :{
         "src": "bench_green.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1,
-        "width_blocks": 2
+        ...TWO_WIDE_SPRITE
     },
     "Bollard" :{
         "src": "bollard.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1,
-        "width_blocks": 1
+        ...ONE_BLOCK_SPRITE
     },
     "Bus_Stop" : {
         "src": "Bus_Stop.png",
@@ -20,21 +74,15 @@ module.exports = {
     },   
     "Computer_With_Table" : {
         "src": "computer_table.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 3,
-        "width_blocks": 1
+        ...THREE_HIGH_SPRITE
     },
     "Couch_Blue" : {
         "src": "couch.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 3,
-        "width_blocks": 1
+        ...THREE_HIGH_SPRITE
     },
     "Fire_Hydrant" :{
         "src": "fire_hydrant.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1,
-        "width_blocks": 1
+        ...ONE_BLOCK_SPRITE
     },
     "Fridge" : {
         "src": "fridge.png",
@@ -44,56 +92,34 @@ module.exports = {
     },
     "Poster_Cruise" : {
         "src": "poster1.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1.75,
-        "width_blocks": 1.75,
-        "not_grounded": true
+        ...POSTER_SPRITE
     },
     "Poster_Cola" : {
         "src": "poster2.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1.75,
-        "width_blocks": 1.75,
-        "not_grounded": true
+        ...POSTER_SPRITE
     },
     "Poster_Gronk" : {
         "src": "poster3.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1.75,
-        "width_blocks": 1.75,
-        "not_grounded": true
+        ...POSTER_SPRITE
     },
     "Sign_01" : {
         "src": "sign1.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1.75,
-        "width_blocks": 1,
-        "not_grounded": true,
-        "idle_animation": true
+        ...getSignData( 1.75 )
     },
     "Sign_02" : {
         "src": "sign2.png",
         "dimensional_alignment": "STANDARD",
-        "height_blocks": 1.75,
-        "width_blocks": 1,
-        "not_grounded": true,
-        "idle_animation": true
+        ...getSignData( 1.75 )
     },
     "Sign_03" : {
         "src": "sign3.png",
         "dimensional_alignment": "STANDARD",
-        "height_blocks": 1,
-        "width_blocks": 1,
-        "not_grounded": true,
-        "idle_animation": true
+        ...getSignData( 1 )
     },
     "Sign_04" : {
         "src": "sign4.png",
         "dimensional_alignment": "STANDARD",
-        "height_blocks": 1,
-        "width_blocks": 1,
-        "not_grounded": true,
-        "idle_animation": true
+        ...getSignData( 1 )
     },
     "Rug_01" : { 
         "src": "rug01.png",
@@ -110,15 +136,11 @@ module.exports = {
     },
     "Sink" : { 
         "src": "sink.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 3,
-        "width_blocks": 1
+        ...THREE_HIGH_SPRITE
     },
     "Small_Table" : {
         "src": "small_table.png",
-        "dimensional_alignment": "STANDARD",
-        "height_blocks": 1,
-        "width_blocks": 1
+        ...ONE_BLOCK_SPRITE
     },
     "Lamppost_1" : {
         "src": "lamppost.png",
@@ -130,110 +152,18 @@ module.exports = {
     // cars
     "car_a" : {
         "src": "car_a.png",
-        "dimensional_alignment": "HORI_VERT",
-        "isCar" : true,
-        "hori_height_blocks": 3,
-        "hori_width_blocks": 4,
-        "vert_height_blocks": 3,
-        "vert_width_blocks": 2,
-        "movement_frames" : { 
-            "FACING_LEFT" : [
-                { "x": 0, "y": 384 },
-                { "x": 0, "y": 576 }
-            ],
-            "FACING_UP" : [
-                { "x": 0, "y": 960 },
-                { "x": 128, "y": 960 }
-            ],
-            "FACING_RIGHT" : [
-                { "x": 0, "y": 0 },
-                { "x": 0, "y": 192 }
-            ],
-            "FACING_DOWN" : [
-                { "x": 0, "y": 768 },
-                { "x": 128, "y": 768 }
-            ]
-        }
+        ...STANDARD_CAR
     },
     "car_b" : {
         "src": "car_b.png",
-        "dimensional_alignment": "HORI_VERT",
-        "isCar" : true,
-        "hori_height_blocks": 3,
-        "hori_width_blocks": 4,
-        "vert_height_blocks": 3,
-        "vert_width_blocks": 2,
-        "movement_frames" : { 
-            "FACING_LEFT" : [
-                { "x": 0, "y": 384 },
-                { "x": 0, "y": 576 }
-            ],
-            "FACING_UP" : [
-                { "x": 0, "y": 960 },
-                { "x": 128, "y": 960 }
-            ],
-            "FACING_RIGHT" : [
-                { "x": 0, "y": 0 },
-                { "x": 0, "y": 192 }
-            ],
-            "FACING_DOWN" : [
-                { "x": 0, "y": 768 },
-                { "x": 128, "y": 768 }
-            ]
-        }
+        ...STANDARD_CAR
     },
     "car_c" : {
         "src": "car_c.png",
-        "dimensional_alignment": "HORI_VERT",
-        "isCar" : true,
-        "hori_height_blocks": 3,
-        "hori_width_blocks": 4,
-        "vert_height_blocks": 3,
-        "vert_width_blocks": 2,
-        "movement_frames" : { 
-            "FACING_LEFT" : [
-                { "x": 0, "y": 384 },
-                { "x": 0, "y": 576 }
-            ],
-            "FACING_UP" : [
-                { "x": 0, "y": 960 },
-                { "x": 128, "y": 960 }
-            ],
-            "FACING_RIGHT" : [
-                { "x": 0, "y": 0 },
-                { "x": 0, "y": 192 }
-            ],
-            "FACING_DOWN" : [
-                { "x": 0, "y": 768 },
-                { "x": 128, "y": 768 }
-            ]
-        }
+        ...STANDARD_CAR
     },
     "car_d" : {
         "src": "car_d.png",
-        "dimensional_alignment": "HORI_VERT",
-        "isCar" : true,
-        "hori_height_blocks": 3,
-        "hori_width_blocks": 4,
-        "vert_height_blocks": 3,
-        "vert_width_blocks": 2,
-        "movement_frames" : { 
-            "FACING_LEFT" : [
-                { "x": 0, "y": 384 },
-                { "x": 0, "y": 576 }
-            ],
-            "FACING_UP" : [
-                { "x": 0, "y": 960 },
-                { "x": 128, "y": 960 }
-            ],
-            "FACING_RIGHT" : [
-                { "x": 0, "y": 0 },
-                { "x": 0, "y": 192 }
-            ],
-            "FACING_DOWN" : [
-                { "x": 0, "y": 768 },
-                { "x": 128, "y": 768 }
-            ]
-        }
+        ...STANDARD_CAR
     }
 }
