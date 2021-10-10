bd36572e27a79c656dd3b28cde52a207f22f16b2
Donderstal <daanonderstal@hotmail.com>
2021-10-10 21:37:18 +0200

    RoadNetwork funcs setting, merging intersection

--- client/src/game/map/RoadNetwork.js
+++ client/src/game/map/RoadNetwork.js
@@ -1,5 +1,7 @@
 const globals = require('../../game-data/globals')
+const { FACING_LEFT, FACING_UP, FACING_RIGHT, FACING_DOWN } = require('../../game-data/globals')
 const { Counter } = require('../../helpers/Counter');
+const { TileSquare } = require('../../helpers/TileSquare');
 const { Road } = require('./roads/Road');
 
 const carCounter = new Counter( 5000, true );
@@ -7,7 +9,15 @@ const carCounter = new Counter( 5000, true );
 class RoadNetwork {
     constructor( roads ) {
         this.roads = [];
-        this.initRoads( roads )
+        this.initRoads( roads );
+
+        this.pendingIntersections = [];
+        this.intersections = [];
+        this.setIntersections( );
+    }
+
+    areDirectionsInList( directions, direction1, direction2) {
+        return directions.indexOf( direction1 ) > -1 && directions.indexOf( direction2 ) > -1
     }
 
     initRoads( roads ) {
@@ -43,6 +53,104 @@ class RoadNetwork {
         }
         else {
             carCounter.resetCounter( );
+        }
+    }
+
+    roadsIntersect( horizontalRoad, verticalRoad ) {
+        return (horizontalRoad.startCol < verticalRoad.leftCol && horizontalRoad.endCol > verticalRoad.rightCol) 
+        || (horizontalRoad.startCol > verticalRoad.leftCol && horizontalRoad.endCol < verticalRoad.rightCol)
+    }
+
+    setIntersections( ) {
+        this.roads.forEach( ( e ) => { 
+            let currentRoad = e;
+            let otherRoads = this.roads.filter( ( e ) => { return e != currentRoad; })
+            otherRoads.forEach( ( otherRoad ) => {
+                if ( currentRoad.alignment == 'HORI' && otherRoad.alignment == 'VERT' && this.roadsIntersect( currentRoad, otherRoad )) {
+                    this.addPendingIntersection(currentRoad, otherRoad); 
+                }
+                else if ( currentRoad.alignment == 'VERT' && otherRoad.alignment == 'HORI' && this.roadsIntersect( otherRoad, currentRoad )) {
+                    this.addPendingIntersection(otherRoad, currentRoad); 
+                }
+            })
+        })
+        console.log(this.pendingIntersections);
+        this.checkPendingIntersections( );
+    }
+
+    addPendingIntersection( horizontalRoad, verticalRoad ) {
+        let FRONT = globals.GAME.FRONT
+        let skip = false;
+        this.pendingIntersections.forEach( ( e ) => {
+            if ( this.areDirectionsInList( e.directions, horizontalRoad.direction, verticalRoad.direction )) {
+                skip = true;
+            }
+        })
+        if ( !skip ) {
+            this.pendingIntersections.push( {
+                'roads': [ verticalRoad, horizontalRoad ],
+                'directions' : [ verticalRoad.direction, horizontalRoad.direction ],
+                'square': new TileSquare( [
+                    FRONT.getTileAtCell( verticalRoad.leftCol, horizontalRoad.topRow ),
+                    FRONT.getTileAtCell( verticalRoad.rightCol, horizontalRoad.topRow ),
+                    FRONT.getTileAtCell( verticalRoad.leftCol, horizontalRoad.bottomRow ),
+                    FRONT.getTileAtCell( verticalRoad.rightCol, horizontalRoad.bottomRow )
+                ] )
+            } )                            
+        }
+    }
+
+    checkPendingIntersections( ) {
+        while ( this.pendingIntersections.length > 0 ) {
+            const pendingIntersection = this.pendingIntersections.shift( );
+            const currentDirections = pendingIntersection.directions;
+            const pendingSquare = pendingIntersection.square;
+            console.log(pendingIntersection);
+            let filteredIntersections = [];
+
+            if ( this.areDirectionsInList(currentDirections, FACING_DOWN, FACING_LEFT) ) {
+                filteredIntersections = this.pendingIntersections.filter( ( e ) => { 
+                    return (
+                        ( this.areDirectionsInList(e.directions, FACING_UP, FACING_LEFT) && e.square.left == pendingSquare.right )
+                        || ( this.areDirectionsInList(e.directions, FACING_DOWN, FACING_RIGHT) && e.square.top == pendingSquare.bottom ) 
+                        || ( this.areDirectionsInList(e.directions, FACING_UP, FACING_RIGHT) && e.square.top == pendingSquare.bottom )
+                    )
+                } );
+            }
+            else if ( this.areDirectionsInList(currentDirections, FACING_DOWN, FACING_RIGHT) ) {
+                filteredIntersections = this.pendingIntersections.filter( ( e ) => { 
+                    return (
+                        ( this.areDirectionsInList(e.directions, FACING_UP, FACING_LEFT) && e.square.bottom == pendingSquare.top )
+                        || ( this.areDirectionsInList(e.directions, FACING_DOWN, FACING_LEFT) && e.square.bottom == pendingSquare.top )
+                        || ( this.areDirectionsInList(e.directions, FACING_UP, FACING_RIGHT) && e.square.left == pendingSquare.right )
+                    )
+                } );
+            } 
+            else if ( this.areDirectionsInList(currentDirections, FACING_UP, FACING_LEFT) ) {
+                filteredIntersections = this.pendingIntersections.filter( ( e ) => { 
+                    return (
+                        ( this.areDirectionsInList(e.directions, FACING_DOWN, FACING_LEFT) && e.square.right == pendingSquare.left )
+                        || ( this.areDirectionsInList(e.directions, FACING_DOWN, FACING_RIGHT) && e.square.top == pendingSquare.bottom )
+                        || ( this.areDirectionsInList(e.directions, FACING_UP, FACING_RIGHT) && e.square.top == pendingSquare.bottom )
+                    )
+                } );
+            }
+            else if ( this.areDirectionsInList(currentDirections, FACING_UP, FACING_RIGHT) ) {
+                filteredIntersections = this.pendingIntersections.filter( ( e ) => { 
+                    return (
+                        ( this.areDirectionsInList(e.directions, FACING_UP, FACING_LEFT) && e.square.bottom == pendingSquare.top )
+                        || ( this.areDirectionsInList(e.directions, FACING_DOWN, FACING_LEFT) && e.square.bottom == pendingSquare.top )
+                        || ( this.areDirectionsInList(e.directions, FACING_DOWN, FACING_RIGHT) && e.square.right == pendingSquare.left )
+                    )
+                } );
+            } 
+
+            console.log([ ...filteredIntersections, pendingIntersection])
+            filteredIntersections.forEach( ( e ) => {
+                let index =  this.pendingIntersections.indexOf( e );
+                this.pendingIntersections.splice( index, 1 )
+            })
+            console.log(this.pendingIntersections)
         }
     }
 }
