6f42b6c51663c87d36a860465dba866f6d2c71cc
Donderstal <daanonderstal@hotmail.com>
2021-10-20 15:56:31 +0200

    checking in/out directions on intersection init

--- client/src/game/map/roads/I_Junction.js
+++ client/src/game/map/roads/I_Junction.js
@@ -11,6 +11,9 @@ class I_Junction {
         this.openLanes = { }
         this.intersectionCars = [];
 
+        this.directionsIn = [];
+        this.directionsOut = [];
+
         this.leftFacingLane = false;
         this.upFacingLane = false;
         this.rightFacingLane = false;
@@ -22,6 +25,47 @@ class I_Junction {
     get rightFacingRoad( ) { return this.roads.filter( ( e ) => { return e.direction == FACING_RIGHT; })[0]; };
     get downFacingRoad( ) { return this.roads.filter( ( e ) => { return e.direction == FACING_DOWN; })[0]; };
 
+    directionEnds( direction ) {
+        return this.directionsIn.indexOf(direction) > -1 && this.directionsOut.indexOf(direction) == -1;
+    }
+
+    directionStarts( direction ) {
+        return this.directionsOut.indexOf(direction) > -1 && this.directionsIn.indexOf(direction) == -1;
+    }
+
+    checkIfRoadsEndOrStartAtIntersection( ) {
+        this.roads.forEach( ( road ) => {
+            switch( road.direction ) {
+                case FACING_LEFT:
+                    if ( this.core.leftColumn != road.endCol )
+                        this.directionsOut.push( FACING_LEFT );
+                    if ( this.core.rightColumn != road.startCol )
+                        this.directionsIn.push( FACING_LEFT );
+                    break;
+                case FACING_UP:
+                    if ( this.core.topRow != road.endRow )
+                        this.directionsOut.push( FACING_UP );
+                    if ( this.core.bottomRow != road.startRow )
+                        this.directionsIn.push( FACING_UP );
+                    break;                
+                case FACING_RIGHT:
+                    if ( this.core.rightColumn != road.endCol )
+                        this.directionsOut.push( FACING_RIGHT );
+                    if ( this.core.lefttColumn != road.startCol )
+                        this.directionsIn.push( FACING_RIGHT );
+                    break; 
+                case FACING_DOWN:
+                    if ( this.core.bottomRow != road.endRow )
+                        this.directionsOut.push( FACING_DOWN );
+                    if ( this.core.topRow != road.startRow )
+                        this.directionsIn.push( FACING_DOWN );
+                    break;       
+                default:
+                    console.log('direction ' + e + ' is not recognized')      
+            }
+        })
+    }
+
     setLanes( ) {
         this.directions.forEach( ( e ) => {
             this.setLane( e )
@@ -32,29 +76,53 @@ class I_Junction {
     }
 
     setLane( direction ) {
-        switch( direction ) {
-            case FACING_LEFT:
-                this.leftFacingLane = new TileSquare(
+        if ( direction == FACING_LEFT ) {
+            if (!this.directionStarts(FACING_LEFT) ) {
+                this.leftFacingInLane = new TileSquare(
                     this.pushTilesToList(this.core.rightColumn, this.leftFacingRoad.topRow, this.core.rightColumn, this.leftFacingRoad.bottomRow, direction)
+                );         
+            }
+            if (!this.directionEnds(FACING_LEFT) ) {
+                this.leftFacingOutLane = new TileSquare(
+                    this.pushTilesToList(this.core.leftColumn - 3, this.leftFacingRoad.topRow, this.core.leftColumn - 3, this.leftFacingRoad.bottomRow, direction)
                 );
-                break;
-            case FACING_UP:
-                this.upFacingLane = new TileSquare(
+            }
+        }
+        if ( direction == FACING_UP) {
+            if ( !this.directionStarts(FACING_UP) ) {
+                this.upFacingInLane = new TileSquare(
                     this.pushTilesToList(this.upFacingRoad.leftCol, this.core.bottomRow, this.upFacingRoad.rightCol, this.core.bottomRow, direction)
                 );
-                break;                
-            case FACING_RIGHT:
-                this.rightFacingLane = new TileSquare(
-                    this.pushTilesToList(this.core.leftColumn, this.rightFacingRoad.topRow, this.core.leftColumn, this.rightFacingRoad.bottomRow, direction)
+            }
+            if (!this.directionEnds(FACING_UP)) {
+                this.upFacingInLane = new TileSquare(
+                    this.pushTilesToList(this.upFacingRoad.leftCol, this.core.topRow - 3, this.upFacingRoad.rightCol, this.core.topRow - 3, direction)
                 );
-                break; 
-            case FACING_DOWN:
-                this.downFacingLane = new TileSquare(
-                    this.pushTilesToList(this.downFacingRoad.leftCol, this.core.topRow, this.downFacingRoad.rightCol, this.core.topRow, direction)
+            }
+        }
+        if ( direction == FACING_RIGHT ) {
+            if (!this.directionStarts(FACING_RIGHT)) {
+                this.rightFacingInLane = new TileSquare(
+                    this.pushTilesToList(this.core.leftColumn - 3, this.rightFacingRoad.topRow, this.core.leftColumn - 3, this.rightFacingRoad.bottomRow, direction)
                 );
-                break;       
-            default:
-                console.log('direction ' + e + ' is not recognized')      
+            }
+            if (!this.directionEnds(FACING_RIGHT)) {
+                this.rightFacingOutLane = new TileSquare(
+                    this.pushTilesToList(this.core.rightColumn, this.rightFacingRoad.topRow, this.core.rightColumn, this.rightFacingRoad.bottomRow, direction)
+                );
+            }         
+        }
+        if ( direction == FACING_DOWN ) {
+            if ( !this.directionStarts(FACING_DOWN) ) {
+                this.downFacingInLane = new TileSquare(
+                    this.pushTilesToList(this.downFacingRoad.leftCol, this.core.topRow - 3, this.downFacingRoad.rightCol, this.core.topRow - 3, direction)
+                );
+            }
+            if ( !this.directionEnds(FACING_DOWN)) {
+                this.downFacingOutLane = new TileSquare(
+                    this.pushTilesToList(this.downFacingRoad.leftCol, this.core.bottomRow, this.downFacingRoad.rightCol, this.core.bottomRow, direction)
+                );
+            }
         }
     }
 
@@ -63,11 +131,11 @@ class I_Junction {
         let tileList = [];
         for( var i = 1; i <= this.laneDepth; i++ ) {
             tileList.push( FRONT.getTileAtCell( 
-                direction == FACING_LEFT ? col1 + i : direction == FACING_RIGHT ? col1 + i : col1, 
-                direction == FACING_UP ? row1 + i : direction == FACING_DOWN ? row1 + i : row1, 
+                direction == FACING_LEFT || direction == FACING_RIGHT ? col1 + i : col1, 
+                direction == FACING_UP || direction == FACING_DOWN ? row1 + i : row1, 
             ), FRONT.getTileAtCell( 
-                direction == FACING_LEFT ? col2 + i : direction == FACING_RIGHT ? col2 + i : col2, 
-                direction == FACING_UP ? row2 + i : direction == FACING_DOWN ? row2 + i : row2, 
+                direction == FACING_LEFT || direction == FACING_RIGHT ? col2 + i : col2, 
+                direction == FACING_UP || direction == FACING_DOWN ? row2 + i : row2, 
             ) )
         }
         return tileList;
